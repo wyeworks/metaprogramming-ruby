4c1816c3f0dc86a6682d3fbb7a7d00f47469dec7
Jeronimo del Mazo <jeronimo.delmazo@uniteus.com>
2024-11-06 15:26:55 -0300

    step 4: adding post conditions and stack example

--- metaprogramming.rb
+++ metaprogramming.rb
@@ -1,53 +1,96 @@
+class PreconditionError < StandardError; end
+class PostconditionError < StandardError; end
+
 class Module
   def pre(&block)
     @current_preconditions ||= []
     @current_preconditions << block
   end
 
+  def post(&block)
+    @current_postconditions ||= []
+    @current_postconditions << block
+  end
+
   def method_added(method_name)
-    return unless @current_preconditions&.any?
+    return unless @current_preconditions&.any? || @current_postconditions&.any?
     # Save the current precondition and reset it
     preconditions = @current_preconditions
     @current_preconditions = []
+    # Save the current postcondition and reset it
+    postconditions = @current_postconditions
+    @current_postconditions = []
 
     # Redefine the method to include the precondition
     original_method = instance_method(method_name)
     define_method(method_name) do |*args, &method_block|
+      puts "Executing preconditions for #{method_name} - preconditions: #{preconditions&.count}"
       # Preconditions execution
-      raise "Some precondition failed for #{method_name}" unless preconditions.all? { |pc| instance_exec(*args, &pc) }
+      raise PreconditionError, "Some precondition failed for #{method_name}" unless preconditions.nil? || preconditions.all? { |pc| instance_exec(*args, &pc) }
+      puts "Executing method #{method_name}"
       # Original method execution
-      original_method.bind(self).call(*args, &method_block)
+      ret = original_method.bind(self).call(*args, &method_block)
+      puts "Executing postconditions for #{method_name}"
+      # Postconditions execution
+      raise PostconditionError, "Some postcondition failed for #{method_name}" unless postconditions.nil? || postconditions&.all? { |pc| instance_exec(*args, &pc) }
+
+      ret
     end
   end
 end
 
-class A
-  pre { |x| x >= 0 }
-  pre { |x| x <= 10 }
-  def method1(x)
-    pp "Running method1 with #{x}, between 0 and 10"
+class Node
+  def initialize(element, next_node)
+    @element = element
+    @next_node = next_node
   end
 
-  pre { |x| x.is_a?(String) }
-  def method2(x)
-    pp x
+  def size
+    next_node.nil? ? 1 : 1 + next_node.size
   end
 
-  pre { true_precondition }
-  def method3(x)
-    pp x
+  attr_reader :element, :next_node
+end
+
+# Sample usage class
+class Stack
+  attr_accessor :current_node, :capacity
+  pre { |capacity| capacity > 0 }
+  post { empty? }
+  def initialize(capacity)
+    @capacity = capacity
+    @current_node = nil
+    puts "Initialized stack with capacity: #{@capacity}"
   end
 
-  pre { false_precondition }
-  def method4(x)
-    pp x
+  pre { !full? }
+  def push(element)
+    puts "Pushing element: #{element}"
+    @current_node = Node.new(element, current_node)
   end
 
-  def true_precondition
-    true
+  pre { !empty? }
+  def pop
+    element = top
+    @current_node = current_node.next_node
+    puts "Popped element: #{element}"
+    element
   end
 
-  def false_precondition
-    false
+  pre { !empty? }
+  def top
+    current_node.element
+  end
+
+  def height
+    empty? ? 0 : current_node.size
+  end
+
+  def empty?
+    current_node.nil?
+  end
+
+  def full?
+    height == capacity
   end
 end
